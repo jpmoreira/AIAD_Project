e5e754b153bbbbc3c1e2703f14ac2f4c4860d35d
Vítor H. Mineiro <vmineiro@me.com>
2014-10-20 17:53:17 +0100

    Hello World Agent!!!

---
+++ src/teste/HelloWorldAgent.java
@@ -0,0 +1,16 @@
+package teste;
+
+import jadex.micro.annotation.Agent;
+import jadex.micro.annotation.AgentBody;
+import jadex.micro.annotation.Description;
+ 
+@Agent
+@Description("An agent that says hello to the world.")
+public class HelloWorldAgent {
+ 
+	@AgentBody
+	public void sayHello(){
+		System.out.println("Hello word!");
+	}
+ 
+}
